85a47eeb4d96b6377ededc5d5c20ee50fc757df4
Pl4yingNight <pl4yingnight@gmail.com>
2022-05-27 12:45:06 +0200

    added master2

---
+++ dns/master2/zones/generate-org-beta-keyes.sh
@@ -0,0 +1,14 @@
+#! /bin/bash
+
+rootpath=/etc/bind/zones
+
+#generate_keys <path> <zone>
+generate_keys () {
+  rm $rootpath/$1/K${2}*
+  dnssec-keygen -a NSEC3RSASHA1 -b 2048 -K $rootpath/$1 -n ZONE $2
+  dnssec-keygen -f KSK -a NSEC3RSASHA1 -b 4096 -K $rootpath/$1 -n ZONE $2
+  ls -l $rootpath/$1/
+}
+
+
+generate_keys org/beta beta.org
